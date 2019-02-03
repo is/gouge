cc42f08269111865c27d9c507e04e7681f2bf9bb
Yu Xin <scaner@gmail.com>
2019-02-03 10:09:35 +0800

    readConfig.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,12 @@
 import { Config as LinkConfig } from "./superlink";
+import { readFileSync } from "fs";
 
 export interface GougeConfig {
   port: number;
   link: LinkConfig;
+}
+
+
+export function readConfig(fn: string): GougeConfig {
+  return JSON.parse(readFileSync(fn).toString("utf8"));
 }
