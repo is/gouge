1e0d929e3756b18951357c0b73a4719e2ec86ae7
Xin Yu <scaner@gmail.com>
2019-02-12 23:12:17 +0800

    webpack config file.

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    "g": "./src/gouge",
+  },
+  target: 'node',
+  mode: 'production',
+  module: {
+    rules: [
+      {test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/},
+    ],
+  },
+  resolve: {
+    extensions:['.ts', '.js'],
+  },
+  plugins: [
+    new webpack.BannerPlugin({banner: '#!/usr/bin/env node', raw:true}),
+  ],
+  externals: {
+    'utf-8-validate': 'utf-8-validate',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname),
+  }
+};
