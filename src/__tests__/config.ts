b1782474211f4ecd10d6132c0857e805c305ca38
Yu Xin <scaner@gmail.com>
2019-02-12 12:46:13 +0800

    completed Gouge.

---
+++ src/__tests__/config.ts
@@ -0,0 +1,30 @@
+import yaml from "js-yaml";
+import { GougeConfig } from "../config";
+
+
+test("Config0", () => {
+  const cfstr = `
+node: t0
+links:
+  - label: t0,t1
+    code: heloworl32103210
+  - label: t0,t3
+    code: heloworlt0t33210
+  - label: t0,t5
+    code: heloworlt0t53210
+`;
+  const cf = <GougeConfig> yaml.load(cfstr);
+
+  expect(cf.links.length).toBe(3);
+  expect(cf.node).toBe("t0");
+  expect(cf.port).toBeUndefined();
+
+  const c0 = cf.links.filter((i) => i.label.split(",")[0] == "t0");
+  expect(c0.length).toBe(3);
+
+  const c1 = cf.links.filter((i) => i.label.split(",")[1] == "t3");
+  expect(c1.length).toBe(1);
+
+  const c2 = cf.links.filter((i) => i.label.split(",")[0] == "tx");
+  expect(c2.length).toBe(0);
+});
